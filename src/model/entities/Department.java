27ddb8234bd90c8c714ae59528312438b853929f
Andre <andrenaatz@yahoo.com.br>
2019-12-01 18:23:35 -0200

    criaçao camada DAO - acessando DB

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 
 public class Department implements Serializable {
 
-	
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
@@ -63,6 +62,4 @@ public class Department implements Serializable {
 	public String toString() {
 		return "Department [id=" + id + ", name=" + name + "]";
 	}
-	
-
 }
